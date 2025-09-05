3ff547b39217c1216a6ab95d53a8d841fd71aaf8
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-05 13:53:07 +0100

    add subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,20 @@
+-- ==============================
+-- Practice Subqueries
+-- ==============================
+
+-- Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
+SELECT properties.name, properties.description, properties.price_per_night, reviews.ratings, reviews.comments
+FROM properties
+INNER JOIN reviews
+ON properties.property_id = reviews.property_id
+WHERE reviews.ratings > 4
+ORDER BY reviews.ratings;
+
+-- Write a correlated subquery to find users who have made more than 3 bookings.
+SELECT user.first_name, user.last_name, booking.start_date
+FROM user
+WHERE user.user_id IN (
+    SELECT bookings.user_id
+    FROM bookings
+    WHERE COUNT(bookings.user_id) > 3
+);
